624a2c0dd02fdf8ebc726e3a3e59254dfa1d8a86
satyamkodale <satyamkodale@gmail.com>
2025-02-08 17:47:08 +0530

    called external module greetings

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/satyamkodale/call-greetings/config"
+	"github.com/satyamkodale/go-greetings/greetings"
+)
+
+func main() {
+	fmt.Println("Version : " + config.Version)
+	//calling external module
+	fmt.Println(greetings.Greeting("Satyam"))
+}
